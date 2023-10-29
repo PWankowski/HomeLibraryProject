aadadbbb5dcfcbf0bce19b4e407cf8af919eee9e
Paweł <pawel.wankowski@wp.pl>
2023-10-29 18:58:01 +0100

    Integration with Google Books Api - added unversioned GoogleBook.class

---
+++ src/main/java/booksProject/googlebooks/entity/GoogleBook.java
@@ -0,0 +1,17 @@
+package booksProject.googlebooks.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GoogleBook {
+
+    private VolumeInfo volumeInfo;
+}
+
+
